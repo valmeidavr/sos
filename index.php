ac695a4e050cb5297ee10bcc384c7585489d84ae
Vinicius Almeida <gvweb.solucoes@gmail.com>
2025-03-11 20:39:04 -0300

    Alterei o echo

--- index.php
+++ index.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Autenticação</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Seja Bem-vindo Dois!</h1>
+    
+    <div class="container-login">
+    
+        <form action="login.php" method="POST">
+            <h3>Tela de Autenticação</h3>
+            <label for="login">Login:</label>
+            <input type="text" name="email" id="email" required>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha" required>
+            <button type="submit">Entrar</button>
+        </form>
+    </div>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,7 @@
+<?php 
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+// Verifica se o email e senha são válidos
+
+?>
